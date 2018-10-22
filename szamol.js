f9885c8118867696d42d8a27c43d81e801d7ee59
nitslaszlo <nitslaszlo@gmail.com>
2018-10-22 15:32:49 +0200

    up

--- szamol.js
+++ szamol.js
@@ -1,4 +1,4 @@
-function szamol() { // eslint-disable-line no-unused-vars
+function szamol() {
     var a = parseFloat(document.getElementById("a").value);
     var b = parseFloat(document.getElementById("b").value);
     var c = parseFloat(document.getElementById("c").value);
